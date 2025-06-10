c2e1b741a8329ce06399e4bcc7e16f570e800ede
Valentin Valencia Valencia <valentinv3v7@gmail.com>
2025-06-10 15:06:39 -0500

    Adds item management data layer
    Introduces DTOs for item creation, response, and update, and implements an ItemRepository
    for managing item data, including fetching, creating, updating, and deleting items via a
    remote service. This change sets up the data access layer for item-related operations.

--- app/src/main/java/com/kata3/kata3app/io/ItemService.kt
+++ app/src/main/java/com/kata3/kata3app/io/ItemService.kt
@@ -2,14 +2,14 @@ package com.kata3.kata3app.io
 
 import android.content.Context
 import com.kata3.kata3app.BuildConfig
-import com.kata3.kata3app.data.repositories.ItemResponse
+import com.kata3.kata3app.data.DTO.ItemCreateRequest
+import com.kata3.kata3app.data.DTO.ItemResponse
+import com.kata3.kata3app.data.DTO.ItemUpdateRequest
 import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface ItemService {
 
@@ -18,6 +18,31 @@ interface ItemService {
         @Header("Authorization") token: String,
         @Query("type") type: String
     ): Response<List<ItemResponse>>
+
+    @GET("items/{id}")
+    suspend fun getItem(
+        @Header("Authorization") token: String,
+        @Path("id") id: String
+    ): Response<ItemResponse>
+
+    @POST("items")
+    suspend fun createItem(
+        @Header("Authorization") token: String,
+        @Body request: ItemCreateRequest
+    ): Response<ItemResponse>
+
+    @PATCH("items/{id}")
+    suspend fun updateItem(
+        @Header("Authorization") token: String,
+        @Path("id") id: String,
+        @Body request: ItemUpdateRequest
+    ): Response<ItemResponse>
+
+    @DELETE("items/{id}")
+    suspend fun deleteItem(
+        @Header("Authorization") token: String,
+        @Path("id") id: String
+    ): Response<Unit>
 
     companion object Factory {
         private const val BASE_URL = BuildConfig.BASE_URL
